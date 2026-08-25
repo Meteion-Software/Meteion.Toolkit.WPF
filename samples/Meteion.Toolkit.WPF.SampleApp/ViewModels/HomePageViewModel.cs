fdcb71f4fb54d5dddc3bdea84e3d5b6d673e4b33
Tylor Pater <tylor.pater@silvercreeksoftware.com>
2026-08-25 16:02:46 -0600

    Add more samples for localization

--- samples/Meteion.Toolkit.WPF.SampleApp/ViewModels/HomePageViewModel.cs
+++ samples/Meteion.Toolkit.WPF.SampleApp/ViewModels/HomePageViewModel.cs
@@ -3,39 +3,62 @@ using CommunityToolkit.Mvvm.Input;
 using Meteion.Toolkit.Localization.Abstractions;
 using Meteion.Toolkit.WPF.SampleApp.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 
-namespace Meteion.Toolkit.WPF.SampleApp.ViewModels
+namespace Meteion.Toolkit.WPF.SampleApp.ViewModels;
+
+public partial class HomePageViewModel : INotifyPropertyChanged
 {
-    public partial class HomePageViewModel : INotifyPropertyChanged
+    private readonly IScopeIdService _scopeIdProvider;
+    private readonly ILocalizationService _localizationService;
+
+    private string _selectedKey = "HomePage_WelcomeMessage";
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public string Title { get; set; } = "Home Page";
+
+    public string ScopeId { get => _scopeIdProvider.Id.ToString(); }
+
+    /// <summary>
+    /// Resource keys the user can pick from, to drive the KeyBinding-via-ComboBox example.
+    /// </summary>
+    public ObservableCollection<string> AvailableKeys { get; } = new(
+        ["HomePage_WelcomeMessage", "ChangeLanguage", "ScopeID", "Feature_Alpha", "Feature_Beta", "Feature_Gamma"]);
+
+    /// <summary>
+    /// The currently selected key for the KeyBinding-via-ComboBox example. Bound
+    /// two-way to a ComboBox, and read by {lx:LocalizedValue KeyBinding={Binding SelectedKey}}.
+    /// </summary>
+    public string SelectedKey
     {
-        private readonly IScopeIdService _scopeIdProvider;
-        private readonly ILocalizationService _localizationService;
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public string Title { get; set; } = "Home Page";
-
-        public string ScopeId { get => _scopeIdProvider.Id.ToString(); }
-
-        public HomePageViewModel(IScopeIdService scopeIdProvider, ILocalizationService localizationService)
+        get => _selectedKey;
+        set
         {
-            _scopeIdProvider = scopeIdProvider;
-            _localizationService = localizationService;
+            if (_selectedKey == value) return;
+            _selectedKey = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedKey)));
         }
+    }
 
-        [RelayCommand]
-        public void SwitchLanguage()
+    public HomePageViewModel(IScopeIdService scopeIdProvider, ILocalizationService localizationService)
+    {
+        _scopeIdProvider = scopeIdProvider;
+        _localizationService = localizationService;
+    }
+
+    [RelayCommand]
+    public void SwitchLanguage()
+    {
+        // Just toggle between EN and JP for now
+        if (_localizationService.CurrentCulture.TwoLetterISOLanguageName == "en")
         {
-            // Just toggle between EN and JP for now
-            if (_localizationService.CurrentCulture.TwoLetterISOLanguageName == "en")
-            {
-                _localizationService.CurrentCulture = new System.Globalization.CultureInfo("ja-JP");
-            }
-            else
-            {
-                _localizationService.CurrentCulture = new System.Globalization.CultureInfo("en-CA");
-            }
+            _localizationService.CurrentCulture = new System.Globalization.CultureInfo("ja-JP");
+        }
+        else
+        {
+            _localizationService.CurrentCulture = new System.Globalization.CultureInfo("en-CA");
         }
     }
 }
