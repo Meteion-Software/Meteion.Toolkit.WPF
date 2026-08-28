df9f8e5ed3a62de0e5194f833f3b37cc7320c1a1
Tylor Pater <tylor.pater@silvercreeksoftware.com>
2026-08-28 11:15:02 -0600

    Introduce InverseBooleanConverter

---
+++ src/Meteion.Toolkit.WPF/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Meteion.Toolkit.WPF.Converters;
+
+/// <summary>
+/// Inverses a boolean.
+/// </summary>
+[ValueConversion(typeof(bool), typeof(bool))]
+public sealed class InverseBooleanConverter : IValueConverter
+{
+    public static readonly InverseBooleanToVisibilityConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return !b;
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        return DependencyProperty.UnsetValue;
+    }
+}
