22864dcaf22f64bfb8de32e955d72ebfcc0ec989
Tylor Pater <tylor.pater@silvercreeksoftware.com>
2026-08-28 10:52:52 -0600

    Add InverseBooleanToVisibilityConverter for WPF
    Added InverseBooleanToVisibilityConverter.cs implementing IValueConverter to map true to Collapsed and false to Visible. Includes static Instance property for reuse. ConvertBack is not supported and returns UnsetValue.

---
+++ src/Meteion.Toolkit.WPF/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Data;
+
+namespace Meteion.Toolkit.WPF.Converters;
+
+/// <summary>
+/// Simply converts a boolean value to a Visibility value. False is converted to Visible, and True is converted to Collapsed. This converter does not support converting back from Visibility to boolean.
+/// </summary>
+[ValueConversion(typeof(bool), typeof(Visibility))]
+public sealed class InverseBooleanToVisibilityConverter : IValueConverter
+{
+    public static readonly InverseBooleanToVisibilityConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return b ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        return DependencyProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, System.Globalization.CultureInfo culture)
+    {
+        return DependencyProperty.UnsetValue;
+    }
+}
